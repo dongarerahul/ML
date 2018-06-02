2c2940f0efedd8ad6131d1ff08a875bdbe10c684
Dongare Rahul <dongare.rahul@gmail.com>
2018-06-02 13:44:42 +0530

    Updated XGBoost algo

--- XGBoost/XGBoost_Churn_prediction.py
+++ XGBoost/XGBoost_Churn_prediction.py
@@ -32,17 +32,37 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Classifer training
 from xgboost import XGBClassifier
+
 classifier = XGBClassifier()
 
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_predict
- 
+from termcolor import colored
+
 classifier.fit(X_train, y_train)
 y_pred = cross_val_predict(classifier, X_test, y_test, cv=10)
-result_metrics = confusion_matrix(y_test, y_pred)
-print(result_metrics)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+predict_accuracy_on_test_set = (cm[0,0] + cm[1,1])/(cm[0,0] + cm[1,1]+cm[1,0] + cm[0,1])
+print(colored('The Accuracy on Test Set is: ', 'blue'), colored(predict_accuracy_on_test_set, 'blue'))
 
-# confusion matrix
-#1521	74
-#197	208
+##########  
+import xgboost as xgb
 
+param = {'booster': 'dart',
+         'max_depth': 5, 'learning_rate': 0.1,
+         'objective': 'binary:logistic', 'silent': True,
+         'sample_type': 'uniform',
+         'normalize_type': 'tree',
+         'rate_drop': 0.1,
+         'skip_drop': 0.5}
+num_round = 50
+dtrain = xgb.DMatrix(X_train, y_train)
+dtest = xgb.DMatrix(X_test, y_test)
+
+bst = xgb.train(param, dtrain, num_round)
+preds = bst.predict(dtest, ntree_limit=num_round)
+
+#y_pred = cross_val_predict(classifier, X_test, y_test, cv=10)
+cm = confusion_matrix(y_test, preds.round())
+print(cm)
