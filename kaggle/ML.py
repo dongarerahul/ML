46d0424ff8d794d29b211a198669c201bd6e5401
Dongare Rahul <dongare.rahul@gmail.com>
2019-07-28 22:18:53 +0530

    Added Titanic Survival Probability Challenge

--- kaggle/ML.py
+++ kaggle/ML.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-housing_data_path = "./sample_submission.csv"
+housing_data_path = "./data/sample_submission.csv"
 housing_data = pd.read_csv(housing_data_path)
-housing_data.describe()
+housing_data.columns
+
+housing_data = housing_data.dropna()
+y = housing_data.pr
+from sklearn.tree import DecisionTreeRegressor
+model = DecisionTreeRegressor(random_state=1)
 
 print("Welcome to VS Code Python !")
